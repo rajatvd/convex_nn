f4cad376a44a1512a4df960fe592284a66f08e35
Aaron Mishkin <aaronpmishkin@gmail.com>
2022-04-18 18:25:21 -0700

    Add missing import.

--- src/convex_nn/private/utils/data/__init__.py
+++ src/convex_nn/private/utils/data/__init__.py
@@ -5,13 +5,14 @@ from .synthetic import (
     gen_classification_data,
     gen_sparse_regression_problem,
 )
-from .transforms import unitize_columns, train_test_split
+from .transforms import unitize_columns, train_test_split, add_bias_col
 
 
 __all__ = [
     "gen_classification_data",
     "gen_regression_data",
     "gen_sparse_regression_problem",
+    "add_bias_col",
     "unitize_columns",
     "train_test_split",
 ]
